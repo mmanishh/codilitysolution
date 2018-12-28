eb525a60219e4a2e10d9ab8f46d8f96e579b8e35
mmanishh <dfrozenthrone@gmail.com>
2018-12-28 14:29:36 +0545

    add euclidean

--- euclidean/num_chocs.py
+++ euclidean/num_chocs.py
@@ -1,4 +1,4 @@
-def chocolates(N,M):
+def chocolates_naive(N,M):
     count =0
     A = list(range(N))
     i = 0
@@ -13,6 +13,17 @@ def chocolates(N,M):
             count +=1
             i = (i+M) % len(A)
             #print(i)
+
+def gcd(p, q):
+  if q == 0:
+    return p
+  return gcd(q, p % q)
+ 
+def lcm(p,q):
+    return p * (q / gcd(p,q))
+ 
+def chocolates(N, M):
+    return lcm(N,M)/M
 
 
 if __name__ == "__main__":
