f38025b322859524a455c0cb1bd8d5ad374c8035
mmanishh <dfrozenthrone@gmail.com>
2018-12-28 12:17:04 +0545

    add stone_wall prob

---
+++ stackqueue/stone_wall.py
@@ -0,0 +1,24 @@
+def solution(H):
+    block_cnt = 0
+     
+    stack = []
+     
+    for height in H:
+        # remove all blocks that are bigger than my height
+        while len(stack) != 0 and stack[-1] > height:
+            stack.pop()
+         
+        if len(stack) != 0 and stack[-1] == height:
+            # we already paid for this size
+            pass
+        else:
+            # new block is required, push it's size to the stack
+            block_cnt += 1
+            stack.append(height)
+             
+    return block_cnt
+
+
+if __name__ == "__main__":
+    A = [8,8,5,7,9,8,7,4,8]
+    print(solution(A))
